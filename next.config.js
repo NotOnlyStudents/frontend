7c88281492df14d38044acbcfb4055089b49d15b
Matteo <matteo16.martini@gmail.com>
2021-03-23 18:06:18 +0100

    close #2 Login Funzionante

---
+++ next.config.js
@@ -0,0 +1,35 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  switch (phase) {
+    case PHASE_DEVELOPMENT_SERVER:
+      return {
+        // environment varibales for local development
+        env: {
+          IDP_DOMAIN: "https://empriolambda.auth.eu-west-1.amazoncognito.com",
+          STRIPE_PUBLIC_KEY: "pk_test_51IHqhuEKthtArr3S4MYSAYFEPiFlioccyA4SjUNArmmdSmK7B05UnMdsNKIu0TCRXADZLVmjEUlqKRIR4D2SWtJ700PVmechEl",
+          USER_POOL_ID: "eu-west-1_GNvsrGORS",
+          USER_POOL_CLIENT_ID: "51v3gjhdbtq2mt0m1fb1asniae",
+          REDIRECT_SIGN_IN: "http://localhost:3004/about.tsx",
+          REDIRECT_SIGN_OUT: "http://localhost:3000/",
+          API_BASE_URL: "http://localhost:3000",
+          AUTH_COOKIE_DOMAIN: "localhost"
+        },
+      };
+    default:
+      return {
+        // environment varibales for production
+        env: {
+          IDP_DOMAIN: "https://empriolambda.auth.eu-west-1.amazoncognito.com",
+          STRIPE_PUBLIC_KEY: "pk_test_51IHqhuEKthtArr3S4MYSAYFEPiFlioccyA4SjUNArmmdSmK7B05UnMdsNKIu0TCRXADZLVmjEUlqKRIR4D2SWtJ700PVmechEl",
+          USER_POOL_ID: "eu-west-1_GNvsrGORS",
+          USER_POOL_CLIENT_ID: "51v3gjhdbtq2mt0m1fb1asniae",
+          REDIRECT_SIGN_IN: "https://shop.annoiato.net/",
+          REDIRECT_SIGN_OUT: "https://shop.annoiato.net/",
+          API_BASE_URL: "https://api.annoiato.net",
+          AUTH_COOKIE_DOMAIN: "shop.annoiato.net"
+        },
+      };
+  }
+
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,83 @@
+import Head from 'next/head'
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect } from 'react';
+import { GetServerSideProps } from "next"
+import { CognitoUser } from "@aws-amplify/auth"
+import { withSSRContext } from 'aws-amplify'
+import { AmplifySignOut } from '@aws-amplify/ui-react'
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+import { useAuthContext } from "context/authContext"
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
 
-export default IndexPage
+export default function Home({
+  _authState,
+  _username
+}: {
+  _authState: AuthState,
+  _username: string | undefined
+}) {
+  const { authState, username, setAuthState, setUsername} = useAuthContext();
+
+  useEffect(() => {
+    setAuthState(_authState);
+    setUsername(_username);
+
+    return onAuthUIStateChange((nextAuthState: AuthState) => {      
+      if (nextAuthState === AuthState.SignedOut) {
+        setAuthState(nextAuthState);
+        setUsername(undefined);
+      }
+    });
+  }, []);
+
+
+
+  return (
+      <div>
+        <div className="container md:mx-auto mt-10">
+          <div className="md:flex rounded-b-xl md:rounded-xl shadow-lg border my-10">
+            <div className="md:w-3/4 md:rounded-xl px-3 md:px-10 py-5 md:py-10">
+            </div>
+
+              <div className="border-t md:mt-8">
+                <div className="flex font-semibold justify-between py-6 text-sm uppercase">
+                  <span>Total cost</span>
+                </div>
+                {authState === AuthState.SignedIn && username ? (
+                  <>
+                    <AmplifySignOut />
+                  </>
+                ) : (
+                  <Link href="/authenticator">
+                    <button className="bg-indigo-500 font-semibold hover:bg-opacity-80 active:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
+                      Login!
+                    </button>
+                  </Link>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { Auth } = withSSRContext(context);
+
+  try {
+    const user: CognitoUser = await Auth.currentAuthenticatedUser();
+
+    return {
+      props: {
+        _authState: AuthState.SignedIn,
+        _username: user.getUsername()
+      }
+    }
+  } catch (err) {
+    return {
+      props: {
+        _authState: AuthState.SignedOut
+      }
+    }
+  }
+}

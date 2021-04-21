4e4d5b940ef52fc435612a022a783f8e26aa8ba3
NOS <cristiano.panighel@studenti.unipd.it>
2021-04-21 22:13:00 +0200

    REMOVE

--- services/order-service/OrderService.ts
+++
@@ -1,8 +0,0 @@
-import { Order, OrderFilter } from 'interfaces/orders/orders';
-
-interface OrderService {
-  getAllOrder(params?: OrderFilter): Promise<Order[]>;
-  getOrderById(id: string): Promise<Order>;
-}
-
-export default OrderService;
--- services/order-service/index.ts
+++
@@ -1,4 +0,0 @@
-import OrderServiceFetch from './OrderServiceFetch';
-import OrderServiceMock from './OrderServiceMock';
-
-export default process.env.NODE_ENV === 'development' ? OrderServiceMock : OrderServiceFetch;

2c61185451226e71aef28f686dc4f3ce8db99475
Matteo <matteo16.martini@gmail.com>
2021-05-02 16:07:37 +0200

    Correct Request

--- interfaces/cart/cart-request.ts
+++ interfaces/cart/cart-request.ts
@@ -3,3 +3,8 @@ import { CartProduct } from 'interfaces/products/product';
 export interface CartGETRequest {
   data: CartProduct[];
 }
+
+export interface CartPostRequest{
+  data: CartProduct[];
+}
+
--- services/cart-service/CartServiceFetch.ts
+++ services/cart-service/CartServiceFetch.ts
@@ -2,16 +2,29 @@ import HTTPRequest from 'lib/HTTPRequest';
 import {
   CartProduct,
 } from 'interfaces/products/product';
-import { CartGETRequest } from 'interfaces/cart/cart-request';
+import { CartGETRequest, CartPostRequest } from 'interfaces/cart/cart-request';
 import CartService from './CartService';
 
 class CartServiceFetch implements CartService {
   getCartProducts = async (token): Promise<CartProduct[]> => {
     const req: HTTPRequest = new HTTPRequest('cart');
-    const res: CartGETRequest = await req.get<CartGETRequest>();
-    //token NON SO DOVE VADA, HEADERS? PARAMS?
+    const headers = {
+      "Authorization": "Bearer " + token,
+      "Content-type": "application/json"
+  }
+    const res: CartGETRequest = await req.get<CartGETRequest>('',headers);
     return res.data;
   };
+
+  /*postCartProducts = async (token): Promise<CartProduct[]> => {
+    const req: HTTPRequest = new HTTPRequest('cart');
+    const body = {
+      "Authorization": "Bearer " + token,
+      "Content-type": "application/json"
+  }
+    const res = await req.post<CartPostRequest>(token);
+    return res.data;
+  };*/
 }
 
 export default CartServiceFetch;
